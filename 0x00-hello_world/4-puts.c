7f9f4c437013aebc4422861c72f82b1cd0076bfb
princess-uki <princessukinebo@gmail.com>
2022-03-10 05:57:37 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,19 +2,22 @@
 
 /**
 
- * main - main block
+*main - entry point
 
- * Return: 0
+*
 
- */
+*Return: Always 0 (Success)
+
+*/
+
+
 
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 
 }
-
