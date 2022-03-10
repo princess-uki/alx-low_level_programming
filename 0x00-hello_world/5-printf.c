d62a664640f2981f67ac439264cf0928ab8fe684
princess-uki <princessukinebo@gmail.com>
2022-03-10 06:23:55 -0800

    5print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-
 /**
-
- * main - main block
-
- * Return: 0
-
- */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-printf("with proper grammar, but the outcome is a piece of art,\n");
-
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
-
 }
-
