7d370336a64389d61624bc680fa1e7a67ed97182
princess-uki <princessukinebo@gmail.com>
2022-03-15 06:10:16 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -18,3 +18,22 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
+#include "main.h"
+/**
+ * main - entry point
+ *Description: print string
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char *s = "_putchar";
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+	return(0);
+}
