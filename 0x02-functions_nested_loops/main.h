06579e33829ffcb29942e24af2b5db21b096d58d
princess-uki <princessukinebo@gmail.com>
2022-03-15 05:55:08 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: print string
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char *s = "_putchar";
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+	return(0);
+}
